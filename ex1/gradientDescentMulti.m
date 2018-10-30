dbe54da056d2b5db66c19e70de7bed58565b2de0
Juan López Rubio <j.lopez.r@gmail.com>
2018-10-30 19:45:26 +0100

    vectorial multivariable gradient

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -16,38 +16,20 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+    
     n = size(X,2);
-    sum = zeros(n);
+    s = zeros(n,1);
     
-    for j=1:n,
-      sum(j)=0;
-      for i=1:m,
-        
-        h = 0;
-        for z=1:n,
-          h = h + theta(z) * X(i,z);
-        endfor
-        
-        sum(j) = sum(j) + (h-y(i))*X(i,j);
-      endfor    
+    H = (X * theta) - y;
+    
+    for j = 1:n
+      T = H;
+      T = T .* X(:,j);
+      s(j) = sum(T);
     endfor
-    
-##    sum1 = 0;
-##    for i=1:m,
-##      h = theta(1) + theta(2) * X(i,2);
-##      sum1 = sum1 + (h-y(i))*X(i,1);
-##    endfor    
-##
-##    sum2 = 0;
-##    for i=1:m,
-##      h = theta(1) + theta(2) * X(i,2);
-##      sum2 = sum2 + (h-y(i))*X(i,2);
-##    endfor
 
-  for j=1:n,
-    theta(j) = theta(j) - alpha * 1/m * sum(j);    
-  endfor
+    s = s .* alpha/m;
+    theta = theta - s;
 
     % ============================================================
 
